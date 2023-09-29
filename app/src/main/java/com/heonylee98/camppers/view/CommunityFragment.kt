0fcd1c0e0105f2931ce7de8cb363494e7797540d
leejiheon <heonylee98@gmail.com>
2023-09-29 20:14:57 +0900

    CommunityFragment Init

--- app/src/main/java/com/heonylee98/camppers/view/CommunityFragment.kt
+++ app/src/main/java/com/heonylee98/camppers/view/CommunityFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.heonylee98.camppers.R
+import com.heonylee98.camppers.databinding.FragmentCommunityBinding
 
 class CommunityFragment : Fragment() {
+    lateinit var fragmentCommunityBinding: FragmentCommunityBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community, container, false)
+        fragmentCommunityBinding = FragmentCommunityBinding.inflate(layoutInflater)
+
+        return fragmentCommunityBinding.root
     }
 }
